72e5a203061441173e9e37052ff8202ab7754374
dbarkowsky <dylanbarkowsky@gmail.com>
2023-10-30 12:50:42 -0700

    Working chat broadcast via websocket

--- server/elysia.ts
+++ server/elysia.ts
@@ -1,20 +1,34 @@
+import { ServerWebSocket } from "bun";
 import { Elysia } from "elysia";
+import { ElysiaWS } from "elysia/ws";
 
 export const app = new Elysia();
+
+const connections: ElysiaWS<ServerWebSocket<any>>[] = [];
 
 app.get("/games", () => "Hello Elysia");
 
 app.ws("/ws", {
   message(ws, message) {
-    ws.send(message);
+    console.log(`Message: ${ws.id}@${ws.remoteAddress} sends ${message}`);
+    // Send message to all connections in the list.
+    connections.forEach((conn) => {
+      conn.send(message);
+    });
   },
   open(ws) {
-    console.log(`Connected: ${ws.id}`);
-    console.log(ws.remoteAddress);
-    console.log(ws.data);
+    console.log(`Connected: ${ws.id}@${ws.remoteAddress}`);
     ws.send("Hello");
+    // Add the connection to the list
+    connections.push(ws);
   },
   close(ws, code, message) {
     ws.send("Goodbye");
+    console.log(`Disconnected: ${ws.id}@${ws.remoteAddress}`);
+    // Remove the connection from the list
+    connections.splice(
+      connections.findIndex((conn) => conn.id === ws.id),
+      1
+    );
   },
 });
