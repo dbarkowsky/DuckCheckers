aac232efb8bdcc66bfa84ef5f63cc6fec5e7ab59
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-11-15 04:05:17 +0000

    remove elysia, replace with express

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
-import { app } from './elysia';
+import app from "./express";
 
-const port = +(process.env.SERVER_PORT ?? 9000);
+const { FRONTEND_URL, SERVER_PORT } = Bun.env;
+
+const port = +(SERVER_PORT ?? 9000);
 
 app.listen(port);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`🦊 Elysia is running at ${FRONTEND_URL ?? `localhost:${port}`}`);
