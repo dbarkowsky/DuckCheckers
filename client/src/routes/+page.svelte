f7723a3aa033649cdd3798fdcb0d13da6cb3f3e8
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2024-02-05 22:04:36 +0000

    frontend formatting

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,23 +1,27 @@
 <script>
-  fetch('http://localhost:22222/api/ongoing').then(response => {
-    console.log(response)
-  })
+	import { onMount } from 'svelte';
 
-  const tiles = new Array(8).fill(new Array(8).fill(0));
+	onMount(() => {
+		fetch('http://localhost:22222/api/ongoing').then((response) => {
+			console.log(response);
+		});
+	});
+
+	const tiles = new Array(8).fill(new Array(8).fill(0));
 </script>
 
 <div class="background">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <div id="board">
-    {#each tiles as row, pindex}
-      <div class="row">
-      {#each row as tile, index}
-        <div class={(pindex + index) % 2 == 0 ? "red" : "black"}>{tile}</div>
-      {/each}
-      </div>
-    {/each}
-  </div>
+	<div id="board">
+		{#each tiles as row, pindex}
+			<div class="row">
+				{#each row as tile, index}
+					<div class={(pindex + index) % 2 == 0 ? 'red' : 'black'}>{tile}</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
@@ -25,17 +29,18 @@
 		background-color: rgb(100, 100, 100);
 	}
 
-  .red {
-    background-color: red;
-  }
+	.red {
+		background-color: red;
+	}
 
-  .black {
-    background-color: black;
-  }
+	.black {
+		background-color: black;
+	}
 
-  .red, .black {
-    display: inline;
-    width: 50px;
-    height: 50px;
-  }
+	.red,
+	.black {
+		display: inline;
+		width: 50px;
+		height: 50px;
+	}
 </style>
